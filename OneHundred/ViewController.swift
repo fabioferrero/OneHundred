de3140317fdd819c832327b3c784db535fff6c9e
Fabio Ferrero <fabio.ferrero@icloud.com>
2017-10-09 16:02:16 +0200

    Add the model into the ViewController and some initialization in the view

--- OneHundred/ViewController.swift
+++ OneHundred/ViewController.swift
@@ -13,13 +13,29 @@ class ViewController: UIViewController
     // The cell grid composed by one hundred buttons
     @IBOutlet weak var cellsGrid: UIStackView!
     
+    // The model for the gameGrid
+    let gameGrid = GameGrid(withRows: 10, andColumns: 10)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var cellCount = 0
+        
+        for stackView in cellsGrid.arrangedSubviews as! [UIStackView] {
+            for button in stackView.arrangedSubviews as! [UIButton] {
+                button.showsTouchWhenHighlighted = true
+                button.accessibilityIdentifier = String(cellCount)
+                cellCount += 1
+            }
+        }
     }
     
-    @IBAction func tapCell(_ cell: UIButton)
+    @IBAction func tapCell(_ button: UIButton)
     {
-        
+        //button.backgroundColor = .red
+        if let buttonID = button.accessibilityIdentifier {
+            print(buttonID)
+        }
     }
     
     
